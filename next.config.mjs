0cfe5fa37e79d7db289ae1450b8bba492ac7aaee
Psolite <info@bulldevelopers.com>
2024-08-25 00:23:52 +0200

    CSP

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     async headers() {
         return [
             {
-                source: "/",
+                source: "/(.*)",
                 headers: [
 
                     {
