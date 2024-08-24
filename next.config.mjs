c1b53ef8e2e5226b2d388fb044e0461da93c57f5
Psolite <info@bulldevelopers.com>
2024-08-24 16:19:26 +0200

    canvas CSP

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+          {
+            source: "/(.)",
+            headers: [
+    
+              {
+                key: "Content-Security-Policy",
+                value: "connect-src 'self' https://api.dscvr.one https://api1.stg.dscvr.one https://*.helius-rpc.com https://api.devnet.solana.com/",
+              },
+    
+            ],
+          },
+        ];
+      },
+    
+};
 
 export default nextConfig;
